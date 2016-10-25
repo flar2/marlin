c5e36e963217f4890d8c3015a110ac32374a7a42
flar2 <asegaert@gmail.com>
2016-10-25 00:04:34 -0400

    wake_gestures: enable sweep up to wake and sweep left to sleep
    These are my preferred settings

--- drivers/input/touchscreen/wake_gestures.c
+++ drivers/input/touchscreen/wake_gestures.c
@@ -37,8 +37,8 @@
 /* Tuneables */
 #define WG_DEBUG		0
 #define WG_DEFAULT		0
-#define S2W_DEFAULT		0
-#define S2S_DEFAULT		0
+#define S2W_DEFAULT		4
+#define S2S_DEFAULT		2
 #define WG_PWRKEY_DUR           180
 
 /* marlin */
